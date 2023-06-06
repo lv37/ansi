d64c661fb8873ad960b2f58d4c7f3ffd1dc535d2
wygsh <132778382+wygsh@users.noreply.github.com>
2023-06-07 00:44:44 +0100

    fix type

--- esc/esc.v
+++ esc/esc.v
@@ -15,12 +15,13 @@ fn (e Sequence) str() string {
 pub fn esc(seq ?Sequence, mode string, args ?Args) string {
 	seq_str := seq.str()
 	args_str :=
-		match typeof(args).name {
-			'[]u8' { (args as []u8).map(it.str()).join(';') }
-			'[]u16' { (args as []u16).map(it.str()).join(';') }
-			else { '' }
-		}
-
+		if args != none {
+			match (args or { []u8{} }).type_name() {
+				'[]u8' { (args as []u8).map(it.str()).join(';') }
+				'[]u16' { (args as []u16).map(it.str()).join(';') }
+				else { '' }
+			}
+		} else { '' }
 	return '\x1b' + seq_str + args_str + mode
 }
 
