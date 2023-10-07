d9e78151316c6a99a51ea6210ab9a811f83ce520
wygsh <132778382+wygsh@users.noreply.github.com>
2023-10-07 17:32:24 +0100

    fix

--- esc/esc.v
+++ esc/esc.v
@@ -8,31 +8,37 @@ pub enum Sequence as u8 {
 
 pub type Args = []u16 | []u8
 
+[inline]
 fn (e Sequence) str() string {
 	return u8(e).ascii_str()
 }
 
 pub fn esc(seq ?Sequence, mode string, args ?Args) string {
 	seq_str := seq.str()
-	args_str := match args or { return '\x1b' + seq_str + mode } {
-		[]u8 { args.map(it.str()).join(';') }
-		[]u16 { args.map(it.str()).join(';') }
+	a := args or { return '\x1b' + seq_str + mode } // compiler bug workaround
+	args_str := match a {
+		[]u8 { a.map(it.str()).join(';') }
+		[]u16 { a.map(it.str()).join(';') }
 	}
 	return '\x1b' + seq_str + args_str + mode
 }
 
+[inline]
 pub fn csi(mode string, args ?Args) string {
 	return esc(Sequence.csi, mode, args)
 }
 
+[inline]
 pub fn dcs(mode string, args ?Args) string {
 	return esc(Sequence.dcs, mode, args)
 }
 
+[inline]
 pub fn osc(mode string, args ?Args) string {
 	return esc(Sequence.osc, mode, args)
 }
 
+[inline]
 pub fn no_seq(mode string, args ?Args) string {
 	return esc(none, mode, args)
 }
